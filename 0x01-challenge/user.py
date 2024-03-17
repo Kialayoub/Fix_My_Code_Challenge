48cabd7a061d15d2a546b8f2655612a584d38409
Kial Ayoub <132392508+Kialayoub@users.noreply.github.com>
2024-03-17 13:43:02 +0000

    Update user.py

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """ 
-User class
+User 
 """
-
 class User():
     """ Documentation """
 
@@ -21,11 +20,9 @@ class User():
         if type(value) is not str:
             raise TypeError("email must be a string")
         self.__email = value
-
-  
     
 if __name__ == "__main__":
 
-    u = User()
-    u.email = "john@snow.com"
-    print(u.email)
+    user = User()
+    user.email = "john@snow.com"
+    print(user.email)
